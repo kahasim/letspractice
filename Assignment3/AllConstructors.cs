d9cdd30e6e2b5bc16938a09a7966a0dd52cae613
kahasim <77129728+kahasim@users.noreply.github.com>
2022-01-26 19:11:06 -0600

    Updated 1/25/22

---
+++ Assignment3/AllConstructors.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment3
+{
+    class AllConstructors
+    {
+        private static string Example;
+        public string Example1="Example1";
+
+        //Default Constructor
+        //Private Constructor prevent variables from being instantiated
+
+        public AllConstructors() { }
+
+        public static void PrivatePrint()
+        {
+            Console.WriteLine("Print private constructor");
+        }
+        static AllConstructors()
+        {
+            Console.WriteLine("Static Constructor ");
+            Example = "This is static with no object created";
+        }
+           
+
+        public AllConstructors(string a)
+        {
+            Console.WriteLine("Parameterize Constructor ");
+            Example = a;
+            Console.WriteLine(a);
+        }
+
+        //copy constructor
+        public AllConstructors(AllConstructors allConstructors)
+        {
+            Console.WriteLine("Copying Constructor");
+            Example = Example1;
+        }
+
+
+    }
+}
---
+++ Assignment3/SumMethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment3
+{
+    class SumMethod : ISuminterface
+    {
+        
+       public int Sum(int a,int b)
+        {
+            int Sum;
+            Sum = a + b;
+            return Sum;
+        }
+
+       public float Sum(float a, float b)
+        {
+            float Sum;
+            Sum = a + b;
+            return Sum;
+        }
+       public double Sum(double a, double b)
+        {
+            double Sum;
+            Sum = a + b;
+            return Sum;
+        }
+
+    }
+}
